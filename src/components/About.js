1d32aba52ef50c46629995f00f221764e700c66c
Yafira <yafira.martinez@lc.cuny.edu>
2022-02-05 17:34:22 -0500

    added content for about section

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function About() {
+	return (
+		<main className={about}>
+			<h2 className={header}>About</h2>
+			<p className={text}>
+				Yafira Martinez is a creative software engineer and designer with an
+				interest in computing, design, emerging technologies, and the
+				intersection of technology and art. Her goal is to build tools that help
+				people and to improve and expand the interaction between humans and
+				computers.
+			</p>
+		</main>
+	);
+}
